9c39182fb183b043978756c5fe3e3394e96c8130
KatharinaHoff <katharina.hoff@gmail.com>
2024-09-15 13:07:12 +0200

    added more fixed

--- postprocessing_scripts/fix_product_names_ncbi.py
+++ postprocessing_scripts/fix_product_names_ncbi.py
@@ -7,6 +7,8 @@ def fix_product_name(product_name):
     # Fix uncharacterized protein
     if product_name.startswith("uncharacterized protein"):
         product_name = "putative protein"
+    # if a product name ends in putative, remove that
+    product_name = re.sub(r'putative$', '', product_name, flags=re.IGNORECASE)
     
     # if a product name ends in uncharacterised, remove that
     product_name = re.sub(r'uncharacterised$', '', product_name, flags=re.IGNORECASE)
@@ -17,23 +19,59 @@ def fix_product_name(product_name):
     if product_name.startswith("hypothetical protein "):
         product_name = product_name.replace("hypothetical protein", "putative protein", 1)
     
+    # Fix proteins protein
+    product_name = re.sub(r'proteins protein', 'protein', product_name)
+
     # Fix plural forms (simple heuristic)
-    product_name = re.sub(r'proteins\b', 'protein', product_name)
+    product_name = re.sub(r'proteins', 'protein', product_name)
 
     # Remove suspicious phrases
     product_name = re.sub(r'truncat(ed|ion)?', '', product_name)
-    product_name = re.sub(r'Truncated ', 'Nonfunctional ', product_name)
-    product_name = re.sub(r'Fragment', '', product_name, flags=re.IGNORECASE)
-    product_name = re.sub(r'partial', '', product_name, flags=re.IGNORECASE)
+    product_name = re.sub(r'Truncated ', r'Nonfunctional ', product_name)
+    product_name = re.sub(r'Fragment', r'', product_name, flags=re.IGNORECASE)
+    product_name = re.sub(r'partial', r'', product_name, flags=re.IGNORECASE)
 
     # Remove strings containing three or more consecutive digits (may be part of a larger string)
-    product_name = re.sub(r'\S*\d{3,}\S*', '', product_name)
+    product_name = re.sub(r'\S*\d{3,}\S*', r'', product_name)
 
     product_name = product_name.replace('_', ' ')  # Replace underscores with spaces
 
     # Change homologs/paralogs to '-like protein'
-    product_name = re.sub(r'\b(Homolog|paralog)\b', '-like protein', product_name)
-    product_name = re.sub(r'\shomolog', '-like proteins', product_name)
+    if not(re.search(r'-like', product_name)):
+        product_name = re.sub(r'\b(Homolog|paralog)\b', r'-like protein', product_name)
+        product_name = re.sub(r'\shomolog', r'-like protein', product_name)
+    else:
+        product_name = re.sub(r'\b(Homolog|paralog)\b', r'', product_name)
+        product_name = re.sub(r'\shomolog', r'', product_name)
+    # also remove like from chloroplastic-like and mitochondrial-like
+    product_name = re.sub(r'chloroplastic-like', r'chloroplastic', product_name)
+    product_name = re.sub(r'mitochondrial-like', r'mitochondrial', product_name)
+    # remove -like proteins from protein -like protein
+    product_name = re.sub(r'protein -like protein', r'protein', product_name)
+
+    # if a product name contains --, make it one -
+    product_name = re.sub(r'--', '-', product_name)
+
+    # someone seriously submitted a homeolog, replace by nothing
+    product_name = re.sub(r'homeolog', '', product_name)
+
+    # make forms singular
+    product_name = re.sub(r'forms', 'form', product_name)
+
+    # if a product name contains two times -like, remove the last -like
+    product_name = re.sub(r'-like(.*)-like', r'\1-like', product_name)
+
+    # if a product name contains -like putative, remove putative
+    product_name = re.sub(r'-like putative', r'-like', product_name)
+
+    # make yippee-like uniformal, if there is no dash, introduce it
+    product_name = re.sub(r'yippee like', r'yippee-like', product_name)
+
+    # if a product name ends in like \d+, remove the digits
+    product_name = re.sub(r'like \d+$', r'like', product_name)
+    # if a product name ends in containing \d+ remove digits
+    product_name = re.sub(r'containing \d+$', r'containing', product_name)
+
     # Shorten long product names to 100 characters
     if len(product_name) > 100:
         # Find the last space before the 100th character
@@ -42,7 +80,7 @@ def fix_product_name(product_name):
         product_name = product_name[:last_space].rstrip()
 
     # Replace 'gene' with 'protein'
-    product_name = re.sub(r'\bgene\b', 'protein', product_name)
+    product_name = re.sub(r'\bgene\b', r'protein', product_name)
 
     # Fix product names starting with "and"
     if re.match(r'\s?and\s', product_name):
@@ -55,13 +93,44 @@ def fix_product_name(product_name):
     # Remove "FOG" from product names
     product_name = product_name.replace("FOG", "")
 
+    # If a product name contains gp, replace the entire product name with "Hypothetical protein"
+    if re.search(r'gp', product_name):
+        product_name = "hypothetical protein"
+
     # Replace "possibly", "residue", and "unnamed" with empty strings or replacements
-    product_name = re.sub(r'\bpossibly\b', '', product_name, flags=re.IGNORECASE)
-    product_name = re.sub(r'\bresidue\b', '', product_name, flags=re.IGNORECASE)
-    product_name = re.sub(r'\bunnamed\b', 'putative protein', product_name, flags=re.IGNORECASE)
+    product_name = re.sub(r'\bpossibly\b', r'', product_name, flags=re.IGNORECASE)
+    product_name = re.sub(r'\bresidue\b', r'', product_name, flags=re.IGNORECASE)
+    product_name = re.sub(r'\bunnamed\b', r'putative protein', product_name, flags=re.IGNORECASE)
 
     # if product ends in family, replace with family member
-    product_name = re.sub(r'family$', 'family member', product_name, flags=re.IGNORECASE)
+    product_name = re.sub(r'family$', r'family member', product_name, flags=re.IGNORECASE)
+
+    # fix plurals
+    product_name = re.sub(r'ases', r'ase', product_name)
+    product_name = re.sub(r'proteins', r'protein', product_name)
+    product_name = re.sub(r'units', r'unit', product_name)
+    product_name = re.sub(r'ac-diamides', r'ac-diamide', product_name)
+    product_name = re.sub(r'chaperonins', r'chaperonin', product_name)
+    product_name = re.sub(r'complexes', r'complex', product_name)
+    product_name = re.sub(r'condensins', r'condensin', product_name)
+    product_name = re.sub(r'copines', r'copine', product_name)
+    product_name = re.sub(r'biotransformers', r'biotransformer', product_name)
+    product_name = re.sub(r'enzymes', r'enzyme', product_name)
+    product_name = re.sub(r'cyclins', r'cyclin', product_name)
+    product_name = re.sub(r'cylophilins', r'cylophilin', product_name)
+    product_name = re.sub(r'channels', r'channel', product_name)
+    product_name = re.sub(r'mutants', r'mutant', product_name)
+    product_name = re.sub(r'receptors', r'receptor', product_name)
+    product_name = re.sub(r'retroposons', r'retroposon', product_name)
+    product_name = re.sub(r'factors', r'factor', product_name)
+    product_name = re.sub(r'members', r'member', product_name)
+    product_name = re.sub(r'antigens', r'antigen', product_name)
+
+
+
+
+
+
 
     # residue Fragilaria_radians.dr # this should be ignored, it's valuable information
     # conserved protein Thalassiosira_delicatula.dr I think this should also be ignored
